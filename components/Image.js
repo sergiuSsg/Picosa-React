8128a4b79be9828a4ef8dbaf721353e4fd701fa1
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-04-08 18:47:04 +0100

    Update Image.js

--- components/Image.js
+++ components/Image.js
@@ -1,51 +1,50 @@
-import React, { useState, useContext } from "react"
-import { AppContext } from "../AppContext"
+import React, {useContext} from "react"
+import {Context} from "../AppContext"
 import PropTypes from "prop-types"
 import useHover from "../hooks/useHover"
+import "../styles.css"
 
+function Image({className, img}) {
+    const {toggleFavorite, cartItems, addToCart, removeFromCart} = useContext(Context)
+    const [hovered, enter, leave] = useHover()
 
-function Image({ className, img }) {
-    const [hovered, ref] = useHover()
-    const { toggleFavorite, addToCart, cartItems, removeFromCart } = useContext(AppContext)
-
-    function heartIcon() {
-        if (img.isFavorite) {
+    function heartIcon(){
+        if (img.isFavorite){
             return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
-        } else if (hovered) {
+        } else if (hovered){
             return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
         }
     }
 
-    function cartIcon() {
-        const alreadyInCart = cartItems.some(item => item.id === img.id)
-        if (alreadyInCart) {
+    function cartIcon(){
+        if (cartItems.some(item => item.id === img.id)){
             return <i className="ri-shopping-cart-fill cart" onClick={() => removeFromCart(img.id)}></i>
-        } else if (hovered) {
+        } else if (hovered){
             return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
         }
     }
 
-    return (
+
+    return(
         <div
-            ref={ref}
+            onMouseEnter={() => enter()}
+            onMouseLeave={() => leave()}
             className={`${className} image-container`}
         >
-            <img src={img.url} className="image-grid" alt="Wallpaper" />
-            {heartIcon()}
+            {heartIcon()} 
             {cartIcon()}
+            <img src={img.url} className="image-grid" alt="Wallpaper"/>
         </div>
     )
 }
 
-
-Image.propTypes = {
+Image.ptopTypes = {
     className: PropTypes.string,
     img: PropTypes.shape({
-        id: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
         url: PropTypes.string.isRequired,
         isFavorite: PropTypes.bool
     })
-
 }
 
 export default Image
