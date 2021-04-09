1882dd3d78b5bfb25481db84286be9df24772073
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-04-09 20:04:01 +0100

    Update AppContext.js

--- AppContext.js
+++ AppContext.js
@@ -1,44 +1,50 @@
-import React, { useState, useEffect } from "react"
-const AppContext = React.createContext()
+import React from "react"
+import "./styles.css"
+import {useState, useEffect} from "react"
 
-function AppContextProvider({ children }) {
+const Context = React.createContext()
 
+function ContextProvider({children}){
+    const url = "https://raw.githubusercontent.com/sergiuSsg/Picosa-React/main/images.json"
     const [allPhotos, setAllPhotos] = useState([])
     const [cartItems, setCartItems] = useState([])
-    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
 
-    useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setAllPhotos(data))
-    }, [])
-
-    function addToCart(newItem) {
-        setCartItems(prevItems => [...prevItems, newItem])
-    }
-    function removeFromCart(id) {
-        setCartItems(prevItems => prevItems.filter(item => item.id !== id))
+    function addToCart(image){
+        setCartItems(prevCart => [...prevCart, image])
     }
 
-    function emptyCart() {
+    function removeFromCart(id){
+        setCartItems(prevCartItems => prevCartItems.filter(item => item.id !== id))
+    }
+    
+    function toggleFavorite(id){    
+        const tempArray = allPhotos.map(photo => {
+            if(photo.id === id) {
+              return {...photo, isFavorite: !photo.isFavorite}
+            }
+
+            return photo
+        })
+        setAllPhotos(tempArray)
+    }
+
+    function emptyCart(){
         setCartItems([])
     }
 
-    function toggleFavorite(id) {
-        const updatedArray = allPhotos.map(photo => {
-            if (photo.id === id) {
-                return { ...photo, isFavorite: !photo.isFavorite }
-            }
-            return photo
-        })
-        setAllPhotos(updatedArray)
-    }
 
-    return (
-        <AppContext.Provider value={{ allPhotos, toggleFavorite, addToCart, removeFromCart, emptyCart, cartItems }}>
-            {children}
-        </AppContext.Provider>
-    )
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+
+    const providerValue = {allPhotos, setAllPhotos, toggleFavorite, cartItems, setCartItems, addToCart, removeFromCart, emptyCart}
+        return(
+            <Context.Provider value={providerValue}>
+                {children}
+            </Context.Provider>
+        )
 }
 
-export { AppContextProvider, AppContext }
+export {ContextProvider, Context}
