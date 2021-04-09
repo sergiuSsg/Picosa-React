e850e2a78a20e8b25188992f580acf2684f91f9f
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-04-09 20:02:27 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
 import React from "react"
-import { Route, Switch } from "react-router-dom"
-
+import {Route, Switch} from "react-router-dom"
 import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
+import Favorites from "./pages/Favorites";
+import Footer from "./components/Footer"
 import "./styles.css"
 import 'remixicon/fonts/remixicon.css'
 
-
-
-function App() {
-  return (
-    <div>
-      <Header />
-
-      <Switch>
-        <Route exact path="/" >
-          <Photos />
-        </Route>
-
-        <Route path="/cart" >
-          <Cart />
-        </Route>
-      </Switch>
-
-    </div>
-  )
+function App() {    
+    return (
+        <div className="page-container">
+            <div className="content-wrap">
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <Photos/>
+                    </Route>
+                    <Route exact path="/cart">
+                        <Cart/>
+                    </Route>
+                    <Route exact path="/favorites">
+                        <Favorites/>
+                    </Route>
+                </Switch>
+            </div>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
